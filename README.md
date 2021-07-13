758e071097948206d9fff919125a36bd4c24f503
xiongzikai <shockzec803@hotmail.com>
2021-07-13 21:55:56 +0800

    sh4wnzec

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # phigros_backup
-Phigros 备份恢复程序，Golang 练习
+> Phigros 备份恢复程序，Golang 学习项目
+
+Tip：目前只适用于 Android 手机
+
+1. 手机连接电脑，选文件传输
+
+2. 解压压缩包，点击`backup.exe`
+
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/eiannone/keyboard"
+	"os"
 	"os/exec"
 )
 
@@ -12,24 +14,36 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(string(output))
+	i := 0
 	for{
-		var inputs int
-		fmt.Println("==================")
-		fmt.Println("请输入要执行的操作对应的数字")
+		if i > 0 {
+			fmt.Println("成功，按任意键退出...")
+			keyboard.GetSingleKey()
+			os.Exit(1)
+		}
+		fmt.Println("=======Phigros 数据备份工具=======\n")
+		fmt.Println("请输入要执行的操作对应的数字\n")
 		fmt.Println("1. 备份")
-		fmt.Println("2. 恢复")
-		fmt.Scanf("%d\n", &inputs)
-		if inputs == 1 {
+		fmt.Println("2. 恢复\n")
+		char, _, err := keyboard.GetSingleKey()
+		if err != nil {
+			panic(err)
+		}
+		if string(char) == "1" {
 			fmt.Println("请打开手机选择备份")
+
 			res := exec.Command("adb", "backup","-f","PhigrosBackup.ab","com.PigeonGames.Phigros")
+			exec.Command("adb", "shell","input","keyevent","82")
+			// adb shell input keyevent 82
 			// 执行命令，并返回结果
 			outputs,err := res.Output()
 			if err != nil {
 				panic(err)
 			}
 			fmt.Println(string(outputs))
+			i++
 		}
-		if inputs == 2 {
+		if string(char) == "2" {
 			fmt.Println("请打开手机选择恢复")
 			res := exec.Command("adb", "restore","PhigrosBackup.ab")
 			// 执行命令，并返回结果
@@ -38,6 +52,7 @@ func main() {
 				panic(err)
 			}
 			fmt.Println(string(outputs))
+			i++
 		}
 	}
 }
